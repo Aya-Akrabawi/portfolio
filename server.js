ae22edf9eb7fb505e6849137cc03968d34d48d67
Aya-Akrabawi <aya.akrabawi.93@hotmail.com>
2020-08-27 15:48:40 +0300

    fis the port to be (process.env.PORT || 3000)

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+const express = require('express');
+// const { response } = require('express');
+const server = express();
+const PORT = process.env.PORT || 3000;
+server.listen(PORT,()=>{
+console.log(`hello ${PORT}`);
+})
+
+server.get('/hii',(request , respond)=>{
+    respond.send('hii you are live now!')
+})
+server.use(express.static('/public'));
