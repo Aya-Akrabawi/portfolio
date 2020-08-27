6c8a4351bbdb1d29691fa3f1363bcf83afb6aad5
Aya-Akrabawi <aya.akrabawi.93@hotmail.com>
2020-08-27 16:01:51 +0300

    fix problem

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 // const { response } = require('express');
 const server = express();
 const PORT = process.env.PORT || 3000;
+
+
 server.listen(PORT,()=>{
 console.log(`hello ${PORT}`);
 })
@@ -10,4 +12,5 @@ console.log(`hello ${PORT}`);
 server.get('/hii',(request , respond)=>{
     respond.send('hii you are live now!')
 })
-server.use(express.static('/public'));
+
+server.use(express.static('./public'));
